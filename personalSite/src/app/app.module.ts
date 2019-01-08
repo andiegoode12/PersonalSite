2ca71e53deae6b9ae8157b1b04b8b04ca30239e1
Andie Goode <andiegoode@Andies-Air.bisonnet.lipscomb.edu>
2019-01-08 12:44:22 -0600

    creating componenets

--- personalSite/src/app/app.module.ts
+++ personalSite/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { CourseworkComponent } from './coursework/coursework.component';
+import { FooterComponent } from './footer/footer.component';
+import { AnimationsComponent } from './animations/animations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    CourseworkComponent,
+    FooterComponent,
+    AnimationsComponent
   ],
   imports: [
     BrowserModule,
